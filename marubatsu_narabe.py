d681dd484d5b3d2d72f424f2ca30155dd8e95c82
h-nakano <nakano.hiroaki@miyazaki-nw.or.jp>
2022-11-22 12:13:34 +0900

    [Add] index

---
+++ marubatsu_narabe.py
@@ -0,0 +1,132 @@
+import tkinter
+import random
+import time
+
+#masu[縦][横]　○は1、×は2
+masu=[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+shirushi=0
+hanteiflg=0
+FNT=("Times New Roman",60)
+
+def masume():
+    # cvs.create_line(200,0,200,600,fill="gray",width=8)    #左の縦線
+    # cvs.create_line(400,0,400,600,fill="gray",width=8)    #右の縦線
+    # cvs.create_line(0,200,600,200,fill="gray",width=8)    #上の横線
+    # cvs.create_line(0,400,600,400,fill="gray",width=8)    #下の横線
+    cvs.delete("all")
+    for i in range(1,3):
+        cvs.create_line(200*i,0,200*i,600,fill="gray",width=8)
+        cvs.create_line(0,i*200,600,i*200,fill="gray",width=8)
+    for y in range(3):
+        for x in range(3):
+            X=x*200
+            Y=y*200
+            if masu[y][x]==1:
+                cvs.create_oval(X+20,Y+20,X+180,Y+180,outline="blue",width=12)
+            if masu[y][x]==2:
+                cvs.create_line(X+20,Y+20,X+180,Y+180,fill="red",width=12)
+                cvs.create_line(X+180,Y+20,X+20,Y+180,fill="red",width=12)
+    if shirushi == 0:
+        cvs.create_text(300,300,text="スタート",fil="navy",font=FNT)
+    cvs.update()
+
+def click(e):
+    global shirushi
+    if shirushi==9:
+        replay()
+        return
+    if shirushi==1 or shirushi==3 or shirushi==5 or shirushi==7:
+        return
+
+    mx=int(e.x/200)
+    my=int(e.y/200)
+    if mx>2:mx=2
+    if my>2:my=2
+    if masu[my][mx]==0:
+        masu[my][mx]=1
+        shirushi = shirushi + 1
+        masume()
+        time.sleep(0.5)
+        hantei()
+        syouhai()
+        if shirushi<9:
+            computer()
+    else:
+        masu[my][mx]=0
+    masume()
+
+def computer():
+    global shirushi
+    while True:
+        x=random.randint(0,2)
+        y=random.randint(0,2)
+        if masu[y][x]==0:
+            masu[y][x]=2
+            shirushi = shirushi + 1
+            masume()
+            time.sleep(0.5)
+            hantei()
+            syouhai()
+            break
+
+def hantei():
+    global hanteiflg
+    hanteiflg = 0
+    #[1]が揃えばプレイヤーの勝利、[2]が揃えばCPUの勝利
+    for n in range(1,3):
+        #縦に並んだかの判定
+        if masu[0][0]==n and masu[1][0]==n and masu[2][0]==n:
+            hanteiflg = n
+        if masu[0][1]==n and masu[1][1]==n and masu[2][1]==n:
+            hanteiflg = n
+        if masu[0][2]==n and masu[1][2]==n and masu[2][2]==n:
+            hanteiflg = n
+        #縦に並んだかの判定
+        if masu[0][0]==n and masu[0][1]==n and masu[0][2]==n:
+            hanteiflg = n
+        if masu[1][0]==n and masu[1][1]==n and masu[1][2]==n:
+            hanteiflg = n
+        if masu[2][0]==n and masu[2][1]==n and masu[2][2]==n:
+            hanteiflg = n
+        #斜めに並んだかの判定
+        if masu[0][0]==n and masu[1][1]==n and masu[2][2]==n:
+            hanteiflg = n
+        if masu[0][2]==n and masu[1][1]==n and masu[2][0]==n:
+            hanteiflg = n
+    if hanteiflg == 1:
+        root.title("プレイヤーの勝利！！")
+    if hanteiflg == 2:
+        root.title("CPUの勝利！！")
+
+def syouhai():
+    global shirushi
+    if hanteiflg==1:
+        cvs.create_text(300,300,text="あなたの勝ち！！",fil="cyan",font=FNT)
+        shirushi=9
+    if hanteiflg==2:
+        cvs.create_text(300,300,text="CPUの勝ち！！",fil="gold",font=FNT)
+        shirushi=9
+    if hanteiflg==0 and hanteiflg==9:
+        cvs.create_text(300,300,text="引き分け",fil="lime",font=FNT)
+
+def replay():
+    global shirushi
+    shirushi = 0
+    for y in range(3):
+        for x in range(3):
+            masu[y][x] = 0
+        masume()
+
+root=tkinter.Tk()
+root.title("○×ゲーム")
+root.resizable(False,False)
+root.bind("<Button>",click)
+cvs=tkinter.Canvas(width=600,height=600,bg="white")
+cvs.pack()
+masume()
+root.mainloop()
+
